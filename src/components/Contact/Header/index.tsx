0440fd82e830ffba8241cf493b769a2d318db506
mayur-clear-match <mayur@clearmatchtalent.com>
2025-07-12 01:12:03 +0530

    PR Feedback

--- src/components/Contact/Header/index.tsx
+++ src/components/Contact/Header/index.tsx
@@ -4,17 +4,16 @@ import { useRouter } from "next/navigation";
 
 function Header() {
   const router = useRouter();
-  const handleAddTask = () => router.push("/contacts/new");
+  const handleAddContact = () => router.push("/contacts/new");
   return (
     <div className="flex justify-between items-center mb-6">
       <h1 className="text-2xl font-bold text-gray-900">Contacts</h1>
 
       <Button
-        onClick={handleAddTask}
-        variant="outline"
+        onClick={handleAddContact}
         className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
-        <Plus className="w-4 h-4" />
+        <Plus className="w-4 h-4 mr-2" />
         Add Contact
       </Button>
     </div>
--- src/components/MyTask/Header/index.tsx
+++ src/components/MyTask/Header/index.tsx
@@ -12,10 +12,9 @@ function Header() {
       <h1 className="text-2xl font-bold text-gray-900">My Task</h1>
       <Button
         onClick={handleAddTask}
-        variant="outline"
         className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
       >
-        <Plus className="w-4 h-4" />
+        <Plus className="w-4 h-4 mr-2" />
         Add Task
       </Button>
     </div>
